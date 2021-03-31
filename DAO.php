8efc6bd1c2b8e0b2ec1f8e149f6f6b158957afe2
djibcat <69469904+djibcat@users.noreply.github.com>
2021-03-31 16:31:17 +0200

    Add files via upload

---
+++ DAO.php
@@ -0,0 +1,22 @@
+<?php
+$DB_NAME = "jeuxvideos_blog";
+$HOST = "localhost";
+$USER = "root";
+$MDP = "";
+
+try
+{
+    $db = new PDO(
+        'mysql:host='.$HOST.';
+        dbname='.$DB_NAME.';
+        charset=utf8',
+        $USER,
+        $MDP);
+
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+?>
